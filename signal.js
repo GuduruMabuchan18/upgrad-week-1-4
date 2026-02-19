4322a81e9bbc4c2cd0adee60ab461bf1167b9d40
GuduruMabuchan18 <mabuchanmabuchan01@gmail.com>
2026-02-19 16:29:34 +0530

    Added to Task 3

---
+++ signal.js
@@ -0,0 +1,20 @@
+// Store traffic signal color
+let signal = "yellow";   // Change value to "red", "yellow", or "green"
+
+// Use switch statement
+switch (signal) {
+    case "red":
+        console.log("Stop");
+        break;
+
+    case "yellow":
+        console.log("Get Ready");
+        break;
+
+    case "green":
+        console.log("Go");
+        break;
+
+    default:
+        console.log("Invalid signal color");
+}
